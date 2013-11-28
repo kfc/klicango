11701050a905751670936a154fced9c74816abce
Yury Bartashevich <kfc@tut.by>
2013-11-28 09:25:05 +0300

    event and calendar improvements

---
+++ profiles/drupal_commons/themes/commons_origins/event-access-denied.tpl.php
@@ -0,0 +1,10 @@
+<div id="event-access-denied-page" title="<?php echo t('Event access denied')?>">
+  <div class="add-friend-text">
+    <?php echo t('You can view only events you are visting or invited to.');?>
+  </div>
+    <div class="person-item">
+        <a class="black-button" href="/user"><?php echo t('View calendar'); ?></a>
+    </div>
+</div>
+
+
