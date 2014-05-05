bfa5988642b6009d2081b03aeb4f3146aa40f1fc
Yauhen Zenko <e.zenko@bylectric.com>
2014-05-05 22:22:58 +0300

    Google analytics

--- profiles/drupal_commons/themes/commons_origins/page.tpl.php
+++ profiles/drupal_commons/themes/commons_origins/page.tpl.php
@@ -95,5 +95,15 @@
     <?php print $create_event_form; ?>
     <?php echo $scripts_modified; ?>
     <div class="modal"></div>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      
+      ga('create', 'UA-50430905-1', 'klicango.fr');
+      ga('send', 'pageview');
+      
+    </script>
   </body>
 </html>
